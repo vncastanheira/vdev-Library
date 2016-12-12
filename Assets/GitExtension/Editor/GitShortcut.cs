9174cb0440b56d0e946b2cf3b31a861000e37993
Vinícius <vncastanheira@gmail.com>
2016-12-12 02:10:27 -0200

    Adding shader and Git Extension

---
+++ Assets/GitExtension/Editor/GitShortcut.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using System.Diagnostics;
+using UnityEditor;
+
+public class GitShortcut : EditorWindow
+{
+	[MenuItem("Git/Open Bash", priority = 1)]
+	public static void ShowBash()
+	{
+		Process foo = new Process();
+		foo.StartInfo.FileName = @"C:\Program Files\Git\git-bash.exe";
+		foo.Start();
+	}
+}
+#endif
