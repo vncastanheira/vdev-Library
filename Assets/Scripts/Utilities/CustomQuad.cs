f66bac2d9cd15c1d0dcd4238c8f704acbbfcc056
Vinicius Epiplon <Vinicius Epiplon>
2017-03-20 15:02:03 -0300

    Added CustomQuad

---
+++ Assets/Scripts/Utilities/CustomQuad.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace vnc.Utilities
+{
+    public class CustomQuad : MonoBehaviour
+    {
+        public Rect rect;
+        public Material customMaterial;
+
+        public void Create(Vector2 rectPos, Vector2 rectSize)
+        {
+            GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            Mesh mesh = (Mesh)Instantiate(quad.GetComponent<MeshFilter>().sharedMesh);
+
+            var vertices = new Vector3[]
+            {
+                rectPos,
+                rectPos + rectSize,
+                new Vector2(rectPos.x + rectSize.x, rectPos.y),
+                new Vector2(rectPos.x, rectPos.y + rectSize.y)
+            };
+            mesh.SetVertices(vertices.ToList());
+            quad.GetComponent<MeshFilter>().mesh = mesh;
+
+            // Fix collider
+            DestroyImmediate(quad.GetComponent<MeshCollider>());
+            quad.AddComponent<MeshCollider>();
+
+            if (customMaterial != null)
+            {
+                var renderer = quad.GetComponent<MeshRenderer>();
+                renderer.material = customMaterial;
+            }
+        }
+    }
+}
+
