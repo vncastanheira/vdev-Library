74b9cdef65b3534e4b8a56cf67fa37bd3906e5a3
Vinícius Naves <vinicius_epiplon@hotmail.com>
2017-03-21 22:36:46 -0300

    Fixed quad positioning

--- Assets/vnc Library/Scripts/Utilities/CustomQuad.cs
+++ Assets/vnc Library/Scripts/Utilities/CustomQuad.cs
@@ -5,21 +5,28 @@ namespace vnc.Utilities
 {
     public static class CustomQuad
     {
+        /// <summary>
+        /// Create a customized Quad from a standart Unity Quad mesh
+        /// </summary>
+        /// <param name="rect">Position and size</param>
+        /// <param name="customMaterial">Apply an user-made material</param>
+        /// <returns></returns>
         public static GameObject Create(Rect rect, Material customMaterial = null)
         {
             GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-            Mesh mesh = (Mesh) Object.Instantiate(quad.GetComponent<MeshFilter>().sharedMesh);
+            Mesh mesh = (Mesh)Object.Instantiate(quad.GetComponent<MeshFilter>().sharedMesh);
 
+            // Each Quad vertex have x: -0.5 and y: -0.5 units
+            Vector2 adjust = (Vector2.one / 2);
             var vertices = new Vector3[]
             {
-                rect.position,
-                rect.position + rect.size,
-                new Vector2(rect.position.x + rect.size.x, rect.position.y),
-                new Vector2(rect.position.x, rect.position.y + rect.size.y)
+                rect.position - adjust,
+                rect.position + rect.size - adjust,
+                new Vector2(rect.position.x + rect.size.x, rect.position.y) - adjust,
+                new Vector2(rect.position.x, rect.position.y + rect.size.y) - adjust
             };
             mesh.SetVertices(vertices.ToList());
             mesh.RecalculateBounds();
-            mesh.RecalculateNormals();
             quad.GetComponent<MeshFilter>().mesh = mesh;
 
             // Fix collider
