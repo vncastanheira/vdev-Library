e833f4a74e2f81d3d03a079e0ee57a04d5fa1059
Vinícius Naves <vinicius_epiplon@hotmail.com>
2017-03-25 10:28:01 -0300

    Added 'NetworkLight'

---
+++ Assets/vnc Library/Scripts/Network/NetworkLight.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace vnc.Network
+{
+    public class NetworkLight : NetworkBehaviour
+    {
+        [SerializeField] Light flashLight;
+        [SerializeField, SyncVar, HideInInspector] bool lightOn;
+        public string ButtonCommand;
+
+        void Update()
+        {
+            if (Input.GetButtonDown(ButtonCommand))
+            {
+                CmdToggleLight();
+            }
+        }
+
+        [Command]
+        public void CmdToggleLight()
+        {
+            RpcSwitchLight();
+        }
+
+
+        [ClientRpc]
+        public void RpcSwitchLight()
+        {
+            lightOn = !lightOn;
+
+            if (lightOn)
+            {
+                flashLight.intensity = 10.0f;
+            }
+            else
+            {
+                flashLight.intensity = 0;
+            }
+        }
+    }
+}
